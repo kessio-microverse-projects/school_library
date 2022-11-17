78e81f9be0e2f59b42191d88bac0b51780c62ffe
unknown <sharonkessio6@gmail.com>
2022-11-17 17:10:58 +0300

    has many relationship classrom side

---
+++ classroom.rb
@@ -0,0 +1,13 @@
+class ClassRoom
+    attr_accessor :label
+    attr_reader :students
+
+    def initialize(label)
+        @label = label
+        @students = []
+    end
+
+    def add_student(student)
+        @students << student
+    end
+end
