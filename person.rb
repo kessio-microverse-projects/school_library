765b5ec0240077d74dd77696ffddb3ffe77ce88b
unknown <sharonkessio6@gmail.com>
2022-11-17 09:59:44 +0300

    fil linter erros

--- person.rb
+++ person.rb
@@ -1,23 +1,21 @@
 class Person
+  attr_reader :id
+  attr_accessor :name, :age
 
-    attr_reader :id
-    attr_accessor :name, :age
+  def initialize(age, name = "unknown", parent_permission = true)
+    @id = Random.rand(1..1000)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
 
-    def initialize(age, name = "unknown", parent_permission = true)
-        @id = Random.rand(1..1000)
-        @age = age
-        @name = name
-        @parent_permission = parent_permission
-    end
-    
-    def can_use_services?
-        @parent_permission || is_of_age?
-           
-    end
+  def can_use_services?
+    @parent_permission || is_of_age?
+  end
 
-    private
+  private
 
-   def is_of_age?
+  def is_of_age?
     return true if @age >= 18
 
     false
