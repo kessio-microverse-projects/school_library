9470937b8064dfe4b2ab5b122fcdcd03249c4763
unknown <sharonkessio6@gmail.com>
2022-11-17 11:50:02 +0300

    fix linter errors

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -2,8 +2,7 @@ require './base_decorator'
 require './person'
 
 class CapitalizeDecorator < BaseDecorator
-    def correct_name
-        @nameable.correct_name.capitalize
-    end
-    
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
 end
