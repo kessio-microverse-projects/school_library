9f3b8b414ae53e7dc27ba605e1c8b0bd2fe47ffd
unknown <sharonkessio6@gmail.com>
2022-11-17 17:38:36 +0300

    person and rental belong to and has many relationship

--- rental.rb
+++ rental.rb
@@ -1,9 +1,10 @@
 class Rental
-    attr_accessor :date :book
+    attr_accessor :date :book :person
 
     def initialize (date)
         @date = date
         book.rentals << self unless book.rentals.include?(self)
+        person.rentals << self unless person.rentals.include?(self)
     end
 
 
